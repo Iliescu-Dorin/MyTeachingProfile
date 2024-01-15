516ba8058b0c06d6d34e7d8f1929bd826afe1adb
Iliescu Mihail Dorin <iliescu.dorin@gmail.com>
2024-01-15 05:45:32 +0200

    new version

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener('DOMContentLoaded', function() {
+  var hamburger = document.querySelector('.hamburger');
+  var navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", function toggleMobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+  hamburger.addEventListener('click', function() {
+      this.classList.toggle('active');
+      navMenu.classList.toggle('active');
+  });
 });
